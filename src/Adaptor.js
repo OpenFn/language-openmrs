3c169b14981875c6be9caccc130649245a19b0f9
taylor <downs.taylor@gmail.com>
2016-02-12 11:46:05 +0200

    add logging to adaptor

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -46,6 +46,7 @@ export function person(personData) {
     const url = resolveUrl(instanceUrl + '/', 'ws/rest/v1/person')
 
     console.log("Posting person:");
+    console.log(body);
 
     return post({ username, password, body, url })
     .then((result) => {
